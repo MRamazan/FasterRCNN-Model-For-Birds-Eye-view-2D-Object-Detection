17025ed7258988f5e515fec46d553123b88f2885
MRamazan <124697078+MRamazan@users.noreply.github.com>
2024-01-23 07:56:36 +0300

    Update VisdroneDataset.py

--- VisdroneDataset.py
+++ VisdroneDataset.py
@@ -1,13 +1,10 @@
+import PIL
 import cv2
-import numpy as np
-import cv2 as cv
 import os
-from time import time
 import torch
 from torchvision import transforms
 from torch.utils import data
-import pandas as pd
-
+from PIL import Image
 class VisdroneDataset(data.Dataset):
     def __init__(self, path):
         self.path = path
@@ -18,6 +15,10 @@ class VisdroneDataset(data.Dataset):
         self.msg = {
             "annotations": []
         }
+        self.obj = 0
+        self.class_id = 0
+        self.targets = []
+        self.count = 0
 
 
 
@@ -44,7 +45,7 @@ class VisdroneDataset(data.Dataset):
                             continue
 
 
-                        msg["annotations"].append([x1, y1, w, h, class_])
+                        msg["annotations"].append([x1, y1, w, h, class_, score])
             return msg
 
 
@@ -60,38 +61,74 @@ class VisdroneDataset(data.Dataset):
         return image
 
     def __getitem__(self, idx):
+        self.count += 1
+        labels = []
+        target = {}
+        boxes = []
         image = self.image_list[idx]
         image_dir = os.path.join(self.image_dir, image)
-        read_img = cv2.imread(image_dir)
+        read_img = PIL.Image.open(image_dir).convert("RGB")
         dir = self.anno_list[idx]
         txt_dir = os.path.join(self.anno_dir, dir)
         anno = self.object_infos(txt_dir)
         lenght = len(anno["annotations"])
         for item in range(0, lenght):
-            class_id = anno["annotations"][item][4]
+            self.class_id = anno["annotations"][item][4]
             x = anno["annotations"][item][0]
             y = anno["annotations"][item][1]
             w = anno["annotations"][item][2]
             h = anno["annotations"][item][3]
-            top_left = (int(x), int(y))
+            score = anno["annotations"][item][5]
+            top_left = int(x), int(y)
             bot_right = (int(x) + int(w), int(y) + int(h))
-            obj = DetectedObject(read_img, class_id, (top_left, bot_right))
-            image = cv2.rectangle(read_img, (top_left), (bot_right), color=(0, 0, 255))
-            if idx % 50 == 0 and idx != 0:
-             cv2.imshow("asd", image)
-             cv2.waitKey(0)
-            else:
-                    cv2.destroyAllWindows()
+            cords = [top_left[0],top_left[1], bot_right[0],bot_right[1]]
+            boxes.append(torch.tensor(cords))
 
-        return obj, class_id
+            labels.append(torch.tensor(int(self.class_id), dtype=torch.int64))
+            target["image_id"] = (torch.as_tensor(idx))
+
+
+            # crop image
+
+
+
+        # recolor, reformat
+        boxes = torch.stack(boxes)
+        labels = torch.stack(labels)
+        label = {
+            'boxes': boxes,
+            "labels" : labels
+
+        }
+        self.targets.append(label)
+        crop = read_img.resize((416, 416))
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        process = transforms.Compose([
+            transforms.ToTensor(),
+            normalize
+        ])
+
+        batch = process(crop)
+
+
+
+
+
+        if self.count == 1:
+            print(self.targets[0])
+            return batch, self.targets[0]
+        else:
+            print(self.targets[self.count - 1])
+            return batch, self.targets[self.count - 1]
 
 class DetectedObject:
     def __init__(self, img, detection_class, box_2d):
 
-        self.img = self.format_img(img, box_2d)
+        self.img = self.normalize_image(img, box_2d)
         self.detection_class = detection_class
 
-    def format_img(self, img, box_2d):
+    def normalize_image(self, img, box_2d):
 
         # Should this happen? or does normalize take care of it. YOLO doesnt like
         # img=img.astype(np.float) / 255
@@ -106,17 +143,12 @@ class DetectedObject:
 
         # crop image
 
-        pt1 = box_2d[0]
-        pt2 = box_2d[1]
-
-        image = cv2.rectangle(img, (pt1), (pt2), color=(0, 0, 255))
-        crop = img[pt1[1]:pt2[1]+1, pt1[0]:pt2[0]+1]
 
 
-        if crop.shape[0] > 0 and crop.shape[1] > 0 and crop.shape[2] > 0:
-            crop = cv2.resize(src=crop, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-        else:
-            print("Warning: Crop height is zero. Skipping resize.")
+
+
+        crop = cv2.resize(src=img, dsize=(416, 416), interpolation=cv2.INTER_CUBIC)
+
 
 
 
